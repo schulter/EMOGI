7631c46a9067005b1cdfcac3c9c683af7e185bbf
Roman Schulte-Sasse <sasse@molgen.mpg.de>
2017-12-20 17:52:03 +0100

    Re-implemented version of own GCN model with more parameters.

--- GCN/my_gcn.py
+++ GCN/my_gcn.py
@@ -1,217 +1,106 @@
 
 import tensorflow as tf
-from utils import *
+#from utils import *
 
 import os, h5py
 import numpy as np
 import pickle
 from datetime import datetime
 import matplotlib.pyplot as plt
+from gcn.layers import GraphConvolution, Dense
+import gcn.utils
+from gcn.models import Model
+from gcn.metrics import masked_accuracy, masked_softmax_cross_entropy
 
+class MYGCN (Model):
+    def __init__(self, placeholders, input_dim, learning_rate=0.1, num_hidden1=20, num_hidden2=40, pos_loss_multiplier=1, weight_decay=5e-4, **kwargs):
+        super(MYGCN, self).__init__()
 
-def load_hdf_data(path):
-    with h5py.File(path, 'r') as f:
-        network = f['network'][:]
-        features = f['features'][:]
-        node_names = f['gene_names'][:]
-        y_train = f['y_train'][:]
-        y_test = f['y_test'][:]
-        if 'y_val' in f:
-            y_val = f['y_val'][:]
-        else:
-            y_val = None
-        train_mask = f['mask_train'][:]
-        test_mask = f['mask_test'][:]
-        if 'mask_val' in f:
-            val_mask = f['mask_val'][:]
-        else:
-            val_mask = None
-    return network, features, y_train, y_val, y_test, train_mask, val_mask, test_mask, node_names
+        # data placeholders
+        self.inputs = placeholders['features']
+        self.input_dim = input_dim
+        # self.input_dim = placeholders['features'].get_shape().as_list()[1]
+        self.output_dim = placeholders['labels'].get_shape().as_list()[1]
+        self.placeholders = placeholders
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 
-class MYGCN:
-    def __init__(self, learning_rate=.01, dropout_prob=.5, num_feature_maps=16, num_classes=7):
-        self.num_feature_maps = num_feature_maps
-        self.dropout_keep_prob = dropout_prob
-        self.learning_rate = learning_rate
-        self.num_outputs = num_classes
+        # model params
+        self.num_hidden1 = num_hidden1
+        self.num_hidden2 = num_hidden2
+        self.weight_decay = weight_decay
+        self.pos_loss_multiplier = pos_loss_multiplier
 
-    def weight_variable(self, shape, name):
-        initial = tf.truncated_normal(shape, stddev=1)
-        return tf.Variable(initial, name=name)
+        # training, prediction and loss functions
+        self.build()
 
-    def bias_variable(self, shape, name):
-        initial = tf.constant(0.1, shape=shape)
-        return tf.Variable(initial, name=name)
+    def _build(self):
+        self.layers.append(GraphConvolution(input_dim=self.input_dim,
+                                            output_dim=self.num_hidden1,
+                                            placeholders=self.placeholders,
+                                            act=tf.nn.relu,
+                                            dropout=True,
+                                            sparse_inputs=True,
+                                            logging=self.logging)
+        )
+        self.layers.append(GraphConvolution(input_dim=self.num_hidden1,
+                                            output_dim=self.num_hidden2,
+                                            placeholders=self.placeholders,
+                                            act=tf.nn.relu,
+                                            dropout=True,
+                                            sparse_inputs=False,
+                                            logging=self.logging)
+        )
+        self.layers.append(GraphConvolution(input_dim=self.num_hidden2,
+                                            output_dim=self.output_dim,
+                                            placeholders=self.placeholders,
+                                            act=lambda x:x,
+                                            dropout=True,
+                                            sparse_inputs=False,
+                                            logging=self.logging)
+        )
 
-    def glorot(self, shape, name=None):
-        """Glorot & Bengio (AISTATS 2010) init."""
-        init_range = np.sqrt(6.0/(shape[0]+shape[1]))
-        initial = tf.random_uniform(shape, minval=-init_range, maxval=init_range, dtype=tf.float32)
-        return tf.Variable(initial, name=name)
+    def _loss(self):
+        # Weight decay loss
+        for var in self.layers[0].vars.values():
+            self.loss += self.weight_decay * tf.nn.l2_loss(var)
 
-    def build_model(self, adj, x, N, C):
-        """
-        Build graph convolutional model.
+        # Cross entropy error
+        self.loss += self.masked_softmax_cross_entropy_weight(self.outputs,
+                                                              self.placeholders['labels'],
+                                                              self.placeholders['labels_mask']
+                                                              )
 
-        Parameters:
-        ----------
-        adj:            Adjecency matrix, the graph.
-                        Shape: (N, N) N = #nodes
+    def _accuracy(self):
+        self.accuracy = masked_accuracy(self.outputs,
+                                        self.placeholders['labels'],
+                                        self.placeholders['labels_mask']
+                                        )
 
-        x:              Input matrix, node features.
-                        Shape: (N, C) C = #node features
-        """
-        A = adj
-        # calculate first hidden layer
-        W_0 = self.glorot((C, self.num_feature_maps), "Weights_Layer_0")
-        #W_0 = weight_variable((C, num_feature_maps), "Weights_Layer_0")
-        A_x = tf.matmul(A, x)
-        H_1_preact = tf.matmul(A_x, W_0)
-        H_1 = tf.nn.relu(H_1_preact)
-        H_1_dropped = tf.nn.dropout(H_1, self.dropout_keep_prob)
-
-        # calculate second hidden layer (output)
-        W_1 = self.glorot((self.num_feature_maps, self.num_outputs), "Weights_Layer_1")
-        #W_1 = weight_variable((num_feature_maps, num_outputs), "Weights_layer_1")
-        A_H1 = tf.matmul(A, H_1_dropped)
-        H_2 = tf.matmul(A_H1, W_1) # shape: (N, num_outputs)
-        H_2_dropped = tf.nn.dropout(H_2, self.dropout_keep_prob)
-        print ("H_2 shape: {}".format(H_2.shape))
-
-        # no activation for H2 as softmax is added by cross-entropy function
-        return H_1, H_2
-
-
-    def construct_computation_graph(self, adj, x, labels, label_mask, num_nodes, num_feat):
-        _, output = self.build_model(adj, x, num_nodes, num_feat)
-        prediction = tf.nn.softmax(output)
-        loss = self.masked_softmax_cross_entropy(output, labels, label_mask)
-        acc = self.masked_accuracy(output, labels, label_mask)
-        train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(loss)
-        return loss, train_step, prediction, acc
-
-    def train_model(self, adj, features, train_y, test_y, val_y, train_mask, test_mask, val_mask, num_nodes, num_feat, node_names, num_epochs=2):
-        keep_prob = tf.placeholder(tf.float32, name="dropout_prob")  # dropout (keep probability)
-        G = tf.placeholder(tf.float32, shape=adj.shape)
-        F = tf.placeholder(tf.float32, shape=features.shape)
-        L = tf.placeholder(tf.float32, shape=train_y.shape)
-        M = tf.placeholder(tf.float32, shape=train_mask.shape)
-        loss_op, train_op, prediction_op, acc_op = self.construct_computation_graph(G, F, L, M, num_nodes, num_feat)
-
-        # Add ops to save and restore all the variables.
-        saver = tf.train.Saver()
-        print ("Graph successfully constructed! Start training...")
-
-        accuracies_test = []
-        accuracies_train = []
-        cross_entropy_test = []
-        with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
-
-            feed_d = {G:adj, F:features, L:train_y, M:train_mask, keep_prob: self.dropout_keep_prob}
-            train_loss = sess.run(loss_op, feed_dict=feed_d)
-            train_acc = sess.run(acc_op, feed_dict=feed_d)
-            print ("Starting training with train acc: {}\tloss: {}".format(train_acc, train_loss))
-            for epoch in range(num_epochs):
-                # optimize
-                feed_d = {G:adj, F:features, L:train_y, M:train_mask, keep_prob: self.dropout_keep_prob}
-                sess.run(train_op, feed_dict=feed_d)
-                # validation accuracy
-                if val_y is None:
-                    feed_val = {G:adj, F:features, L:test_y, M:test_mask, keep_prob: 1.}
-                else:
-                    feed_val = {G:adj, F:features, L:val_y, M:val_mask, keep_prob: 1.}
-                val_acc = sess.run(acc_op, feed_dict=feed_val)
-                # training accuracy
-                feed_train_acc = {G:adj, F:features, L:train_y, M:train_mask, keep_prob: 1.}
-                train_acc = sess.run(acc_op, feed_dict=feed_train_acc)
-                # validation loss
-                val_loss = sess.run(loss_op, feed_dict=feed_val)
-                # training loss
-                train_loss = sess.run(loss_op, feed_dict=feed_train_acc)
-                accuracies_train.append(train_acc)
-                accuracies_test.append(val_acc)
-                cross_entropy_test.append(val_loss)
-                print ("[Epoch {}]\tTraining Acc: {:.2f}\tVal Acc: {:.2f}\tTraining Loss: {:.2f}\tVal Loss: {:.2f}".format(epoch, train_acc, val_acc, train_loss, val_loss))
-            feed_d = {G:adj, F:features, L:test_y, M: test_mask}
-            test_acc = sess.run(acc_op, feed_d)
-            predictions = sess.run(prediction_op, feed_d)
-            print ("Final Accuracy on Test Set: {}".format(test_acc))
-            self.finish_after_training(saver, sess, accuracies_test, accuracies_train, cross_entropy_test, predictions, node_names)
-
-
-    def load_and_evaluate(self, path, adj, features, test_y, test_mask):
-        keep_prob = tf.placeholder(tf.float32, name="dropout_prob")  # dropout (keep probability)
-        G = tf.placeholder(tf.float32, shape=adj.shape)
-        F = tf.placeholder(tf.float32, shape=features.shape)
-        L = tf.placeholder(tf.float32, shape=test_y.shape)
-        M = tf.placeholder(tf.float32, shape=test_mask.shape)
-        loss_op, train_op, prediction_op, acc_op = self.construct_computation_graph(G, F, L, M, adj.shape[0], features.shape[1])
-        saver = tf.train.Saver()
-        with tf.Session() as sess:
-            saver.restore(sess, path)
-            feed_d = {G:adj, F:features, L:test_y, M:test_mask, keep_prob: 1.}
-            prediction = sess.run(prediction_op, feed_dict=feed_d)
-            test_acc = sess.run(acc_op, feed_dict=feed_d)
-            test_loss = sess.run(loss_op, feed_dict=feed_d)
-
-        print ("Test Accuracy: {}\tTest Loss: {}".format(test_acc, test_loss))
-        return prediction
-
-    def get_weights(self, path, adj, features):
-        W_0 = self.glorot((features.shape[1], self.num_feature_maps), "Weights_Layer_0")
-        W_1 = self.glorot((self.num_feature_maps, self.num_outputs), "Weights_Layer_1")
-        saver = tf.train.Saver()
-        with tf.Session() as sess:
-            saver.restore(sess, path)
-            return W_0.eval(sess), W_1.eval(sess)
-
-    def get_hidden_activation(self, path, adj, features):
-        tf.reset_default_graph()
-        G = tf.placeholder(tf.float32, shape=adj.shape)
-        F = tf.placeholder(tf.float32, shape=features.shape)
-        H_1_op, H_2_op = self.build_model(G, F, adj.shape[0], features.shape[1])
-        saver = tf.train.Saver()
-        with tf.Session() as sess:
-            saver.restore(sess, path)
-            print ("Model successfully loaded!")
-            H_1, H_2 = sess.run([H_1_op, H_2_op], feed_dict={G:adj, F:features})
-            return H_1, H_2
-
-
-    def normalize_adj(self, adj):
-        """Symmetrically normalize adjacency matrix."""
-        adj = sp.coo_matrix(adj)
-        rowsum = np.array(adj.sum(1))
-        d_inv_sqrt = np.power(rowsum, -0.5).flatten()
-        d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
-        d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
-        return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
-
-
-    def preprocess_adj(self, adj):
-        """Preprocessing of adjacency matrix for simple GCN model and conversion to tuple representation."""
-        adj_normalized = self.normalize_adj(adj + sp.eye(adj.shape[0]))
-        return adj_normalized
-
-    def masked_softmax_cross_entropy(self, preds, labels, mask):
-        """Softmax cross-entropy loss with masking."""
-        loss = tf.nn.softmax_cross_entropy_with_logits(logits=preds, labels=labels)
-
-        print ("loss shape: {}".format(loss.shape))
+    def masked_softmax_cross_entropy_weight(self, scores, labels, mask):
+        """Softmax cross-entropy loss with masking and weight for positives."""
+        loss = tf.nn.softmax_cross_entropy_with_logits(logits=scores, labels=labels)
+        loss += labels[:,0]*self.pos_loss_multiplier
         mask = tf.cast(mask, dtype=tf.float32)
         mask /= tf.reduce_mean(mask)
         loss *= mask
         return tf.reduce_mean(loss)
 
-    def masked_accuracy(self, preds, labels, mask):
-        """Accuracy with masking."""
-        correct_prediction = tf.equal(tf.argmax(preds, 1), tf.argmax(labels, 1))
-        accuracy_all = tf.cast(correct_prediction, tf.float32)
-        mask = tf.cast(mask, dtype=tf.float32)
-        mask /= tf.reduce_mean(mask)
-        accuracy_all *= mask
-        return tf.reduce_mean(accuracy_all)
+    def predict(self):
+        return tf.nn.softmax(self.outputs)
+
+    def save(self, path, sess=None):
+        if not sess:
+            raise AttributeError("TensorFlow session not provided.")
+        saver = tf.train.Saver(self.vars)
+        save_path = saver.save(sess, path)
+        print("Model saved in file: %s" % save_path)
+
+    def load(self, path, sess=None):
+        if not sess:
+            raise AttributeError("TensorFlow session not provided.")
+        saver = tf.train.Saver(self.vars)
+        saver.restore(sess, path)
+        print("Model restored from file: %s" % path)
 
     def finish_after_training(self, saver, sess, accuracies_test, accuracies_train, cross_entropy_test, predictions, node_names):
         if not os.path.isdir('training'):
@@ -244,20 +133,3 @@ class MYGCN:
         plt.plot(accuracies_train, color='red', label='Accuracy on the training set')
         plt.legend(loc="lower right")
         fig.savefig(save_path + 'plot.png', dpi=400)
-
-
-if __name__ == "__main__":
-    gcn = MYGCN(num_classes=2, dropout_prob=0.25, num_feature_maps=36)
-    print ("Loading Data...")
-    #adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_data('cora')
-    adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask, node_names = load_hdf_data('../data/preprocessing/legionella_gcn_input.h5')
-    num_nodes = adj.shape[0]
-    num_feat = features.shape[1]
-
-    # Some preprocessing
-    #features = preprocess_features(features)
-    adj = gcn.preprocess_adj(adj)
-    adj = adj.todense()
-    #features = features.todense()
-    adj = np.asarray(adj)
-    gcn.train_model(adj, features, y_train, y_test, y_val, train_mask, test_mask, val_mask, num_nodes, num_feat, node_names, num_epochs=20)
---
+++ GCN/train_gcn.py
@@ -0,0 +1,176 @@
+import argparse
+import os, h5py
+import numpy as np
+import pickle
+from datetime import datetime
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import gcn.utils
+from my_gcn import MYGCN
+from scipy.sparse import csr_matrix, lil_matrix
+from gcn.models import GCN
+import time
+
+def load_hdf_data(path):
+    with h5py.File(path, 'r') as f:
+        network = f['network'][:]
+        features = f['features'][:]
+        node_names = f['gene_names'][:]
+        y_train = f['y_train'][:]
+        y_test = f['y_test'][:]
+        if 'y_val' in f:
+            y_val = f['y_val'][:]
+        else:
+            y_val = None
+        train_mask = f['mask_train'][:]
+        test_mask = f['mask_test'][:]
+        if 'mask_val' in f:
+            val_mask = f['mask_val'][:]
+        else:
+            val_mask = None
+    return network, features, y_train, y_val, y_test, train_mask, val_mask, test_mask, node_names
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train GCN model and save to file')
+    parser.add_argument('-e', '--epochs', help='Number of Epochs',
+                        dest='epochs',
+                        default=150,
+                        type=int
+                        )
+    parser.add_argument('-lr', '--learningrate', help='Learning Rate',
+                        dest='lr',
+                        default=.1,
+                        type=float
+                        )
+    parser.add_argument('-s', '--support', help='Neighborhood Size in Convolutions',
+                        dest='support',
+                        default=1,
+                        type=int
+                        )
+    parser.add_argument('-h1', '--hidden_units_1', help='Number of feature maps for layer 1',
+                        dest='hidden1',
+                        default=20,
+                        type=int
+                        )
+    parser.add_argument('-h2', '--hidden_units_2', help='Number of feature maps for layer 2',
+                        dest='hidden2',
+                        default=40,
+                        type=int
+                        )
+    parser.add_argument('-lm', '--loss_mul',
+                        help='Number of times, false negatives are weighted higher than false positives',
+                        dest='loss_mul',
+                        default=1,
+                        type=float
+                        )
+    parser.add_argument('-wd', '--weight_decay', help='Weight Decay',
+                        dest='decay',
+                        default=5e-4,
+                        type=float
+                        )
+    parser.add_argument('-do', '--dropout', help='Dropout Percentage',
+                        dest='dropout',
+                        default=.5,
+                        type=float
+                        )
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    print ("Loading Data...")
+    args = parse_args()
+    data = load_hdf_data('../data/simulation/simulated_input_legionella.h5')
+    adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask, node_names = data
+    num_nodes = adj.shape[0]
+    num_feat = features.shape[1]
+
+    features = gcn.utils.preprocess_features(lil_matrix(features))
+
+    # create placeholders and other stuff for TF
+    poly_support = args.support
+    if poly_support > 1:
+        support = gcn.utils.chebyshev_polynomials(adj, poly_support)
+        num_supports = 1 + poly_support
+    else:
+        support = [gcn.utils.preprocess_adj(adj)]
+        num_supports = 1
+    placeholders = {
+        'support': [tf.sparse_placeholder(tf.float32) for _ in range(num_supports)],
+        'features': tf.sparse_placeholder(tf.float32, shape=tf.constant(features[2], dtype=tf.int64)),
+        'labels': tf.placeholder(tf.float32, shape=(None, y_train.shape[1])),
+        'labels_mask': tf.placeholder(tf.int32),
+        'dropout': tf.placeholder_with_default(0., shape=()),
+        'num_features_nonzero': tf.placeholder(tf.int32)  # helper variable for sparse dropout
+    }
+
+    # create session, train and save afterwards
+    with tf.Session() as sess:
+        model = MYGCN(placeholders=placeholders,
+                      input_dim=features[2][1],
+                      learning_rate=args.lr,
+                      weight_decay=args.decay,
+                      num_hidden1=args.hidden1,
+                      num_hidden2=args.hidden2,
+                      pos_loss_multiplier=args.loss_mul,
+                      dropout=args.dropout,
+                      logging=True)
+
+        def evaluate(features, support, labels, mask, placeholders):
+            feed_dict_val = gcn.utils.construct_feed_dict(features, support, labels, mask, placeholders)
+            loss, acc = sess.run([model.loss, model.accuracy], feed_dict=feed_dict_val)
+            return loss, acc
+
+        def predict(features, support, labels, mask, placeholders):
+            feed_dict_pred = gcn.utils.construct_feed_dict(features, support, labels, mask, placeholders)
+            pred = sess.run(model.predict(), feed_dict=feed_dict_pred)
+            return pred
+
+        sess.run(tf.global_variables_initializer())
+        cost_val = []
+        for epoch in range(args.epochs):
+            t = time.time()
+            feed_dict = gcn.utils.construct_feed_dict(features, support, y_train, train_mask, placeholders)
+            feed_dict.update({placeholders['dropout']: args.dropout})
+            # Training step
+            outs = sess.run([model.opt_op, model.loss, model.accuracy], feed_dict=feed_dict)
+
+            # Validation
+            cost, acc = evaluate(features, support, y_test, test_mask, placeholders)
+            cost_val.append(cost)
+
+            # Print results
+            print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(outs[1]),
+                  "train_acc=", "{:.5f}".format(outs[2]), "val_loss=", "{:.5f}".format(cost),
+                  "val_acc=", "{:.5f}".format(acc))
+        print("Optimization Finished!")
+
+        # Testing
+        test_cost, test_acc = evaluate(features, support, y_test, test_mask, placeholders)
+        print("Test set results:", "cost=", "{:.5f}".format(test_cost),
+        "accuracy=", "{:.5f}".format(test_acc))
+
+        # do final prediction and save model to directory
+
+        # predict node classification
+        predictions = predict(features, support, y_test, test_mask, placeholders)
+
+        # save model and predictions
+        root_dir = '../data/GCN/training'
+        if not os.path.isdir(root_dir):
+            os.mkdir(root_dir)
+            print ("Created Training Subdir")
+        date_string = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        os.mkdir(os.path.join(root_dir, date_string))
+        save_path = os.path.join(root_dir, date_string)
+        model_save_path = os.path.join(save_path, 'model.ckpt')
+        print ("Save model to {}".format(model_save_path))
+        path = model.save(model_save_path, sess=sess)
+
+        with open(os.path.join(save_path, 'predictions.tsv'), 'w') as f:
+            f.write('ID\tName\tProb_pos\tProb_neg\n')
+            for pred_idx in range(predictions.shape[0]):
+                f.write('{}\t{}\t{}\t{}\n'.format(node_names[pred_idx, 0],
+                                                  node_names[pred_idx, 1],
+                                                  predictions[pred_idx,0],
+                                                  predictions[pred_idx,1])
+                        )
