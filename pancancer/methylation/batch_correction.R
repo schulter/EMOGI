fdc9838d1e7636fab6cb77d39353074a0c276fad
Roman Schulte-Sasse <sasse@molgen.mpg.de>
2018-11-22 12:20:52 +0100

    Don't run combat when some batches have only one sample.

--- pancancer/methylation/batch_correction.R
+++ pancancer/methylation/batch_correction.R
@@ -12,7 +12,15 @@ pheno = read.table(phenotype_path, sep = '\t', header = TRUE)
 sample_matrix = as.matrix(samples)
 pheno$batch <- as.factor(pheno$batch)
 
+freqs_of_batches = as.data.frame(table(pheno$batch))
+
 # run ComBat using the batch variable for adjustment
-modCombat = model.matrix(~1, data = pheno)
-adjusted_data = ComBat(dat = sample_matrix, batch = as.factor(pheno$batch), mod = modCombat, par.prior = TRUE, prior.plots = FALSE)
-write.table(adjusted_data, output_path, sep = '\t')
+if (length(unique(pheno$batch)) > 1 && min(freqs_of_batches$Freq) > 1) { # something to adjust
+    print (length(unique(pheno$batch)))
+  modCombat = model.matrix(~1, data = pheno)
+  adjusted_data = ComBat(dat = sample_matrix, batch = as.factor(pheno$batch), mod = modCombat, par.prior = TRUE, prior.plots = FALSE)
+  write.table(adjusted_data, output_path, sep = '\t')
+} else { # everything comes from the same batch, nothing to do
+    print("Don't normalize because same batch!")
+  write.table(sample_matrix, output_path, sep = '\t')
+}
